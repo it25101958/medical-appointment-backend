0a3ab3b18930ccd6eed775e1bcd63e3dbeb2f083
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-04-22 16:16:01 +0530

    refactor: updated endpoint from "/users" to "/api/v1/users"

--- src/main/java/com/medical/appointment/controller/UserController.java
+++ src/main/java/com/medical/appointment/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("api/v1/users")
 public class UserController {
 
     private final UserService userService;

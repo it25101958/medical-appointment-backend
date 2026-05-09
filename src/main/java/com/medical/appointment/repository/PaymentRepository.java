c0e4eb3dd97e6e510a821d7dc89c15a8d7205f14
IT25101952 <it25101952@my.sliit.lk>
2026-05-09 11:45:03 +0530

    feat: add PaymentRepository extending JpaRepository

--- src/main/java/com/medical/appointment/repository/PaymentRepository.java
+++ src/main/java/com/medical/appointment/repository/PaymentRepository.java
@@ -1,4 +1,10 @@
 package com.medical.appointment.repository;
 
-public class PaymentRepository {
+import com.medical.appointment.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
 }

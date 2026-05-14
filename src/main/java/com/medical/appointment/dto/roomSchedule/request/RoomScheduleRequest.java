147254222c2ed46478b16b641f250df4044fb0c1
Thashmina P.G.D <it25101986@my.sliit.lk>
2026-05-14 20:13:14 +0530

    Create RoomSchedule Request

---
+++ src/main/java/com/medical/appointment/dto/roomSchedule/request/RoomScheduleRequest.java
@@ -0,0 +1,29 @@
+package com.medical.appointment.dto.roomSchedule.request;
+
+import com.medical.appointment.model.enums.DayOfWeek;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+public class RoomScheduleRequest {
+
+    @NotNull(message = "Doctor ID is required")
+    private Integer doctorId;
+
+    @NotNull(message = "Room ID is required")
+    private Integer roomId;
+
+    @NotNull(message = "Appointment ID is required")
+    private Integer appointmentId;
+
+    @NotNull(message = "Day of week is required")
+    private DayOfWeek dayOfWeek;
+
+    @NotNull(message = "Start time is required")
+    private LocalTime startTime;
+
+    @NotNull(message = "End time is required")
+    private LocalTime endTime;
+}

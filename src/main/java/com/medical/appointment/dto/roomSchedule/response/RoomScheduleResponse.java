55c1ce3ea66e107db1aa3e7196099f2befee4d84
Thashmina P.G.D <it25101986@my.sliit.lk>
2026-05-14 20:15:11 +0530

    Create RoomSchedule Response

---
+++ src/main/java/com/medical/appointment/dto/roomSchedule/response/RoomScheduleResponse.java
@@ -0,0 +1,24 @@
+package com.medical.appointment.dto.roomSchedule.response;
+
+import com.medical.appointment.model.enums.DayOfWeek;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomScheduleResponse {
+    private Integer roomScheduleId;
+    private String roomNumber;
+    private String doctorName;
+    private String appointmentNumber;
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}

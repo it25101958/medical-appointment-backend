bd3137450270f74309d4b3d24eb92a5c4596db5c
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-05-16 00:15:48 +0530

    refactor: remove empty spaces

--- src/main/java/com/medical/appointment/dto/auth/request/LoginRequest.java
+++ src/main/java/com/medical/appointment/dto/auth/request/LoginRequest.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginRequest {
-
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;

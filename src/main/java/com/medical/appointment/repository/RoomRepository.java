de25b547fcc56e5b8e6b326c80f4c5a9db968ed9
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-05-16 16:19:02 +0530

    refactor: code refactor

--- src/main/java/com/medical/appointment/repository/RoomRepository.java
+++ src/main/java/com/medical/appointment/repository/RoomRepository.java
@@ -1,10 +1,16 @@
 package com.medical.appointment.repository;
 
 
+import com.medical.appointment.model.Doctor;
 import com.medical.appointment.model.Room;
+import com.medical.appointment.model.RoomSchedule;
+import com.medical.appointment.model.enums.DayOfWeek;
 import com.medical.appointment.model.enums.RoomStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,6 +23,20 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
     List<Room> findByRoomType(String roomType);
     Optional<Room> findByRoomNumber(String roomNumber);
     boolean existsByRoomNumber(String roomNumber);
+
+    @Query("""
+    SELECT rs FROM RoomSchedule rs
+    WHERE rs.doctor = :doctor
+      AND rs.dayOfWeek = :dayOfWeek
+      AND rs.startTime <= :startTime
+      AND rs.endTime >= :endTime
+""")
+    Optional<RoomSchedule> findDoctorActiveShift(
+            Doctor doctor,
+            DayOfWeek dayOfWeek,
+            LocalTime startTime,
+            LocalTime endTime
+    );
 
 
 }

b054a8bc520050f749e3e820e526c79e8dfeb794
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2026-05-16 06:59:29 +0530

    feat: add laborder item mapping and laboratory relationship

--- src/main/java/com/medical/appointment/model/LabOrder.java
+++ src/main/java/com/medical/appointment/model/LabOrder.java
@@ -3,6 +3,8 @@ package com.medical.appointment.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 
 @Entity
 @Table(name = "lab_order")
@@ -19,5 +21,14 @@ public class LabOrder {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "appointment_id", referencedColumnName = "appointmentId", nullable = false)
     private Appointment appointment;
+
+    @OneToMany(mappedBy = "labOrder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LabOrderItem> items;
+
+    @ManyToOne
+    @JoinColumn(name = "laboratory_id")
+    private Laboratory laboratory;
+
+
 
 }

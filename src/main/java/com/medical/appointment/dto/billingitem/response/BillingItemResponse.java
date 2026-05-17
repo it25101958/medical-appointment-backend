da998906aa609933959b871287bad72aaade87f9
IT25101952 <it25101952@my.sliit.lk>
2026-05-17 08:12:47 +0530

    feat: add medicationId , labTestId composition and basic response fields to BillingItemResponse

---
+++ src/main/java/com/medical/appointment/dto/billingitem/response/BillingItemResponse.java
@@ -0,0 +1,25 @@
+package com.medical.appointment.dto.billingitem.response;
+
+import com.medical.appointment.model.enums.BillingItemType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+public class BillingItemResponse {
+
+    private Integer billingItemId;
+    private Integer billingId;
+    private BillingItemType itemType;
+    private String description;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal totalPrice;
+
+
+    private Integer medicationId;
+    private Integer labTestId;
+
+}
